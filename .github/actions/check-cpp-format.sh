fae38c88eec602265e55c2846555f585124341b0
thomas-bc <49786685+thomas-bc@users.noreply.github.com>
2024-05-28 12:50:02 -0700

    Debug with GitHub RUNNER_DEBUG argument

--- .github/actions/check-cpp-format.sh
+++ .github/actions/check-cpp-format.sh
@@ -10,11 +10,16 @@ if [[ -n $FOLDERS_LIST ]]; then
     folders=$FOLDERS_LIST
 fi
 
+# --name-only flag unless debug is set
+if [[ -z $RUNNER_DEBUG ]]; then
+    GIT_DIFF_FLAGS="--name-only"
+fi
+
 # Loop over the folders
 for folder in $folders; do
     echo "[INFO] Processing folder: $folder"
     find $folder -name "*.cpp" -o -name "*.hpp" | fprime-util format --stdin --no-backup -q
-    find $folder -name "*.cpp" -o -name "*.hpp" | xargs git diff --exit-code --name-only
+    find $folder -name "*.cpp" -o -name "*.hpp" | xargs git diff --exit-code $GIT_DIFF_FLAGS
     if [ $? -ne 0 ]; then
         echo "[ERROR] Files in the folder $folder are not formatted correctly."
         FORMAT_ERROR=1
