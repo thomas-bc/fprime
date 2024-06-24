92ba380ca5163b83725a4abc0d4f5636509fd937
Nathan Chow <nathan.chow@jpl.nasa.gov>
2024-06-24 10:57:17 -0400

    Move PDU header definitions to PDU header hpp file

--- Fw/Ccsds/Cfdp/FilePacket.hpp
+++ Fw/Ccsds/Cfdp/FilePacket.hpp
@@ -21,79 +21,18 @@ namespace Cfdp
 class FilePacket
 {
 
+  /*
+   * Enum declarations.
+   */
   public:
-    //! @brief PDU type options.
-    //!
-    enum class Type
-    {
-      FILE_DIRECTIVE = 0, //!< Indicates a file directive PDU.
-      FILE_DATA = 1, //!< Indicates a file data PDU.
-    };
-
-    //! @brief PDU direction options.
-    //!
-    //! Used to perform PDU forwarding.
-    //!
-    enum class Direction
-    {
-      TOWARD_RECEIVER = 0, //!< Indicates a PDU towards the file receiver.
-      TOWARD_SENDER = 1, //!< Indicates a PDU towards the file sender.
-    };
-
-    //! @brief Transmission mode options.
-    //!
-    //! If the transmission mode is ACKNOWLEDGED, protocol entity will use
-    //! reliable file transfer procedures. Acknowledged transmission mode
-    //! requires duplex transmission paths.
-    //!
-    enum class TransmissionMode
-    {
-      ACKNOWLEDGED = 0, //!< Indicates acknowledged transmission mode.
-      UNACKNOWLEDGED = 1, //!< Indicates unacknowledged transmission mode.
-    };
-
-    //! @brief CRC flag options.
-    //!
-    //! If the flag is PRESENT, the sending protocol entity will calculate the
-    //! CRC for each outgoing PDU and append it to the data field. The receiving
-    //! protocol entity will calculate the CRC for each incoming PDU.
-    //!
-    enum class CrcFlag
-    {
-      NOT_PRESENT = 0, //!< Indicates a CRC is not included in the data field.
-      PRESENT = 1, //!< Indicates a CRC is included in the data field.
-    };
-
-    //! @brief Large file flag options.
-    //!
-    //! If the file is a LARGE_FILE, then its size cannot be represented in an
-    //! unsigned 32-bit integer. All files of unbounded size shall be flagged as
-    //! LARGE_FILE, all other files shall be flagged as SMALL_FILE.
-    //!
-    enum class LargeFileFlag
-    {
-      SMALL_FILE = 0, //!< Indicates a 'small' file.
-      LARGE_FILE = 1, //!< Indicates a 'large' file.
-    };
-
-    //! @brief Segmentation control options.
-    //!
-    //! If flag is PRESERVED, then record boundaries are preserved in file data
-    //! segmentation.
-    //!
-    enum class SegmentationControl
-    {
-      NOT_PRESERVED = 0, //!< Indicates record boundaries are not preserved.
-      PRESERVED = 1, //!< Indicates record boundaries are preserved.
-    };
-
-    //! @brief Segment metadata flag options.
-    //!
-    enum class SegmentMetadataFlag
-    {
-      NOT_PRESENT = 0, //!< Indicates segment metadata is not in the PDU.
-      PRESENT = 1, //!< Indicates segment metadata is in the PDU.
-    };
+    // Header related enums
+    enum class Type;
+    enum class Direction;
+    enum class TransmissionMode;
+    enum class CrcFlag;
+    enum class LargeFileFlag;
+    enum class SegmentationControl;
+    enum class SegmentMetadataFlag;
 
   public:
     //! @brief A class defining the Length Value (LV) object format.
--- Fw/Ccsds/Cfdp/Header.hpp
+++ Fw/Ccsds/Cfdp/Header.hpp
@@ -17,6 +17,79 @@ namespace Fw
 namespace Cfdp
 {
 
+//! @brief PDU type options.
+//!
+enum class FilePacket::Type
+{
+  FILE_DIRECTIVE = 0, //!< Indicates a file directive PDU.
+  FILE_DATA = 1, //!< Indicates a file data PDU.
+};
+
+//! @brief PDU direction options.
+//!
+//! Used to perform PDU forwarding.
+//!
+enum class FilePacket::Direction
+{
+  TOWARD_RECEIVER = 0, //!< Indicates a PDU towards the file receiver.
+  TOWARD_SENDER = 1, //!< Indicates a PDU towards the file sender.
+};
+
+//! @brief Transmission mode options.
+//!
+//! If the transmission mode is ACKNOWLEDGED, protocol entity will use
+//! reliable file transfer procedures. Acknowledged transmission mode
+//! requires duplex transmission paths.
+//!
+enum class FilePacket::TransmissionMode
+{
+  ACKNOWLEDGED = 0, //!< Indicates acknowledged transmission mode.
+  UNACKNOWLEDGED = 1, //!< Indicates unacknowledged transmission mode.
+};
+
+//! @brief CRC flag options.
+//!
+//! If the flag is PRESENT, the sending protocol entity will calculate the
+//! CRC for each outgoing PDU and append it to the data field. The receiving
+//! protocol entity will calculate the CRC for each incoming PDU.
+//!
+enum class FilePacket::CrcFlag
+{
+  NOT_PRESENT = 0, //!< Indicates a CRC is not included in the data field.
+  PRESENT = 1, //!< Indicates a CRC is included in the data field.
+};
+
+//! @brief Large file flag options.
+//!
+//! If the file is a LARGE_FILE, then its size cannot be represented in an
+//! unsigned 32-bit integer. All files of unbounded size shall be flagged as
+//! LARGE_FILE, all other files shall be flagged as SMALL_FILE.
+//!
+enum class FilePacket::LargeFileFlag
+{
+  SMALL_FILE = 0, //!< Indicates a 'small' file.
+  LARGE_FILE = 1, //!< Indicates a 'large' file.
+};
+
+//! @brief Segmentation control options.
+//!
+//! If flag is PRESERVED, then record boundaries are preserved in file data
+//! segmentation.
+//!
+enum class FilePacket::SegmentationControl
+{
+  NOT_PRESERVED = 0, //!< Indicates record boundaries are not preserved.
+  PRESERVED = 1, //!< Indicates record boundaries are preserved.
+};
+
+//! @brief Segment metadata flag options.
+//!
+enum class FilePacket::SegmentMetadataFlag
+{
+  NOT_PRESENT = 0, //!< Indicates segment metadata is not in the PDU.
+  PRESENT = 1, //!< Indicates segment metadata is in the PDU.
+};
+
 //! @brief A CFDP PDU header.
 //!
 class FilePacket::Header
